4883fc9b141137e2e3078249816df2a0387f8840
Superdragon <superdragon@caselibre.fr>
2023-01-05 01:32:13 +0100

    Adjusting Retro_Overload setting on phosphor_monochrome theme

--- phosphor_monochrome/schema/php/Overload.php
+++ phosphor_monochrome/schema/php/Overload.php
@@ -1,4 +1,4 @@
 <?php
 
 if (! (isset($imgfilter) && $imgfilter))
-        $imgfilter = 'brightness(0.5) sepia(1) hue-rotate(200deg) saturate(1) contrast(1.5) brightness(3) blur(0.2px)';
+        $imgfilter = 'brightness(0.5) sepia(1) hue-rotate(200deg) saturate(1.5) contrast(1.5) brightness(3) blur(0.6px) brightness(0.9)';
