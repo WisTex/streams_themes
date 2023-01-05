b0906a00069cd780a828590c16c02da250cf6a69
Superdragon <superdragon@caselibre.fr>
2023-01-05 01:17:48 +0100

    Adjusting Retro_Overload setting on phosphor_orange theme

--- phosphor_green/schema/php/Overload.php
+++ phosphor_green/schema/php/Overload.php
@@ -1,4 +1,4 @@
 <?php
 
 if (! (isset($imgfilter) && $imgfilter))
-        $imgfilter = 'brightness(0.5) sepia(1) hue-rotate(90deg) saturate(3) contrast(1.4) brightness(3) blur(0.2px)';
+        $imgfilter = 'brightness(0.5) sepia(1) hue-rotate(80deg) saturate(3) contrast(1.4) brightness(2.2) blur(0.6px)';
--- phosphor_orange/schema/php/Overload.php
+++ phosphor_orange/schema/php/Overload.php
@@ -1,4 +1,4 @@
 <?php
 
 if (! (isset($imgfilter) && $imgfilter))
-        $imgfilter = 'brightness(0.5) sepia(1) hue-rotate(355deg) saturate(3) contrast(1.2) brightness(1.8) blur(0.2px)';
+        $imgfilter = 'brightness(0.4) sepia(1) hue-rotate(5deg) saturate(3.5) contrast(1.2) brightness(1.8) blur(0.6px)';
